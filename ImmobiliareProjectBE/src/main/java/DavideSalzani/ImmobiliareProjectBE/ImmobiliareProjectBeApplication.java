bc0c96f44f74ad496a214a4173174841db9e0532
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-30 14:05:44 +0100

    Initial commit

---
+++ ImmobiliareProjectBE/src/main/java/DavideSalzani/ImmobiliareProjectBE/ImmobiliareProjectBeApplication.java
@@ -0,0 +1,13 @@
+package DavideSalzani.ImmobiliareProjectBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobiliareProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobiliareProjectBeApplication.class, args);
+	}
+
+}
---
+++ ImmobiliareProjectBE/src/test/java/DavideSalzani/ImmobiliareProjectBE/ImmobiliareProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package DavideSalzani.ImmobiliareProjectBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobiliareProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
