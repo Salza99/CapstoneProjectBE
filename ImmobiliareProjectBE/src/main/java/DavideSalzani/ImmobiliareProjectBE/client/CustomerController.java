f5698ae511af16e285f09d2a7bb42a498570287a
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-12-04 01:03:52 +0100

    CRUD estate and customer done

--- ImmobiliareProjectBE/src/main/java/DavideSalzani/ImmobiliareProjectBE/client/CustomerController.java
+++ ImmobiliareProjectBE/src/main/java/DavideSalzani/ImmobiliareProjectBE/client/CustomerController.java
@@ -1,4 +1,40 @@
 package DavideSalzani.ImmobiliareProjectBE.client;
 
+import DavideSalzani.ImmobiliareProjectBE.address.Address;
+import DavideSalzani.ImmobiliareProjectBE.client.payload.NewCustomerDTO;
+import DavideSalzani.ImmobiliareProjectBE.exceptions.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public Customer createCustomer(@RequestBody @Validated NewCustomerDTO body, BindingResult validation){
+        if (validation.hasErrors()){
+            throw new BadRequestException(validation.getAllErrors());
+        }else {
+            return customerService.createNewCustomer(body);
+        }
+    }
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Customer> getAll(){
+        return customerService.getAllCustomer();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Customer getSingleAddress(UUID id){
+        return customerService.findSingleCustomer(id);
+    }
 }
--- ImmobiliareProjectBE/src/main/java/DavideSalzani/ImmobiliareProjectBE/estate/EstateController.java
+++ ImmobiliareProjectBE/src/main/java/DavideSalzani/ImmobiliareProjectBE/estate/EstateController.java
@@ -1,12 +1,41 @@
 package DavideSalzani.ImmobiliareProjectBE.estate;
 
+import DavideSalzani.ImmobiliareProjectBE.client.Customer;
+import DavideSalzani.ImmobiliareProjectBE.estate.payloads.NewEstateDTO;
+import DavideSalzani.ImmobiliareProjectBE.exceptions.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/estates")
 public class EstateController {
     @Autowired
     EstateService estateService;
 
 
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Estate createNewEstate(@RequestBody @Validated NewEstateDTO body, BindingResult validation){
+        if (validation.hasErrors()) {
+            throw new BadRequestException(validation.getAllErrors());
+        }else {
+            return estateService.createEstate(body);
+        }
+    }
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estate> getAll(){
+        return estateService.getAllEstate();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Estate getSingleAddress(UUID id){
+        return estateService.getById(id);
+    }
 }
